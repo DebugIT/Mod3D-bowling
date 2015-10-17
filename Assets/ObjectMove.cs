6a4b075206ba0133584faa0aa6bee81c219fb4db
Piotr Mścichowski <piotr.mscichowski@hybris.com>
2015-10-17 16:21:59 +0200

    [Lab1]First commit, added gitignore file, added second scene and new script for steering ball via keyboard

---
+++ Assets/ObjectMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectMove : MonoBehaviour {
+
+    public Renderer renderer;
+
+    // Use this for initialization
+	void Start () {
+        renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            renderer.material.color = Color.black;
+            // Change color
+        }
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 position = gameObject.transform.position;
+            const float speed = 0.05f;
+            float translateX = Input.GetAxis("Mouse X") * speed;
+            float translateY = Input.GetAxis("Mouse Y") * speed;
+            position.x += translateX;
+            position.y += translateY;
+            gameObject.transform.position = position;
+        }
+	}
+}
