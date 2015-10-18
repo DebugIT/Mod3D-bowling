10fcf3c42fee98d8bad4a354f2e466bb27fed98a
Piotr Mścichowski <piotr.mscichowski@hybris.com>
2015-10-18 11:54:00 +0200

    [Lab1]Further improvements, added mouse controll to throw a bowl, added second Camera

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class Ball : MonoBehaviour {
+	float speed = 1.0f;
+	public float thrust;
+	public Rigidbody rb;
+	protected CharacterController controller;
+
+	public float moveSpeed = 2f;
+	public float turnSpeed = 90f;
+	
+	protected Vector3 move = Vector3.zero;
+	
+	// Use this for initialization
+	public virtual void Start () {
+		controller = GetComponent<CharacterController> ();
+		
+		if (!controller) {
+			Debug.LogError ("Character controller not assigned");
+			enabled = false;
+		}
+		//rb = gameObject.AddComponent<Rigidbody> ();
+		//rb = GetComponent<Rigidbody>();
+	}
+	
+	public virtual void FixedUpdate() {
+	}
+	
+	// Update is called once per frame
+	public virtual void Update () {
+
+		controller.SimpleMove(move );
+
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		if(Input.GetKey(KeyCode.RightArrow)){
+			
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if(Input.GetKey(KeyCode.UpArrow)){
+			
+			transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.position += Vector3.down * speed * Time.deltaTime;	 
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			//rb.AddForce (transform.forward * thrust, ForceMode.Impulse)
+
+		}
+		
+	}
+	
+}
---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : Ball {
+
+	Vector3 start = Vector3.zero;
+	Vector3 end = Vector3.zero;
+	// Use this for initialization
+	public override void Start () {
+		base.Start ();
+	}
+
+	// Update is called once per frame
+	public override void Update () {
+		//test ();
+
+		if (Input.GetMouseButton (0)) {
+			transform.Rotate (0f, Input.GetAxis ("Mouse X") * turnSpeed * Time.deltaTime, 0f);
+		}
+
+		if (Input.GetMouseButton (1)) {
+		
+				Vector3 moveStrength = new Vector3(0F, 0f,Mathf.Abs(-Input.GetAxis("Mouse X")) * Mathf.Abs(-Input.GetAxis("Mouse X")) * Time.deltaTime);
+			move = moveStrength;
+
+			move.Normalize();
+			move = transform.TransformDirection (move);
+		}
+
+
+
+		//transform.Rotate (0f, Input.GetAxis ("Mouse X") * turnSpeed * Time.deltaTime, 0f);
+
+		//Vector3 moveStrength = new Vector3(Input.GetAxis("Mouse X") * turnSpeed * Time.deltaTime, 0f);
+
+		Camera.main.transform.forward = transform.forward;
+
+		//move = new Vector3(Input.GetAxis ("Horizontal"), 0f, Input.GetAxis ("Vertical"));
+		
+		//move.Normalize();
+		
+		//move = transform.TransformDirection (move);
+
+
+		base.Update ();
+	}
+
+	/*void test() {
+		RaycastHit hit = new RaycastHit();
+		Vector3 distance = Vector3.zero;
+		
+		if (Input.GetMouseButtonDown (0)) {
+			if (Physics.Raycast (camera.ScreenPointToRay (Input.mousePosition), hit)) {
+				start = hit.point;
+			}
+		}
+		
+		if (Input.GetMouseButtonUp (0)) {
+			if (Physics.Raycast (camera.ScreenPointToRay (Input.mousePosition), hit)) {
+				end = hit.point;
+				distance = end - start;
+				Debug.Log ("Distance in X = " + Mathf.Abs (distance.x));
+			}
+		}
+	}*/
+}
