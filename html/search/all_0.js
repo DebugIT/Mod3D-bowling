a242c2a0680e807fe7d2a54711add5aff6b86fea
Marek Nalepa <marek.nalepa@gmail.com>
2015-10-21 19:17:40 +0200

    [Lab1] Cleaned up repository

---
+++ html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['ball',['Ball',['../class_ball.html',1,'']]],
+  ['ballmovement',['BallMovement',['../class_ball_movement.html',1,'']]]
+];
---
+++ html/search/variables_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['possiblecolors',['possibleColors',['../class_color_change.html#a82d62c6de3e5c99613d876af2de00627',1,'ColorChange']]]
+];
