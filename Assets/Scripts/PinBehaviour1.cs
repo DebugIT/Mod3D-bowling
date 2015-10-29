4796c1624ef77083e4b93592009a8227a99c7be0
Piotr Mścichowski <piotr.mscichowski@gmail.com>
2015-10-29 12:26:51 +0100

    [LAB2] Initial behaviour for pin

---
+++ Assets/Scripts/PinBehaviour1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinBehaviour1 : MonoBehaviour {
+
+	Rigidbody pinBody;
+	Vector3 vecUp;
+	// Use this for initialization
+	void Start () {
+		pinBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (vecUp.y <= 10 && vecUp.y >= 0) {
+			vecUp = Vector3.up * 2;
+		}
+
+		if (vecUp.y < 0 && vecUp.y > 10) {
+			vecUp = Vector3. down * 2;
+		}
+
+		pinBody.MovePosition (transform.position +  (-transform.forward) * 4);
+
+	}
+}
